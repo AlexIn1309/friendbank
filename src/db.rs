98fe0c6ef01fd0ff2881bf291404ab6bb82b7051
AlexIn1309 <moran.graciano.cesar@gmail.com>
2026-01-19 23:51:49 -0600

    change login to main

--- src/db.rs
+++ src/db.rs
@@ -4,4 +4,4 @@ pub async fn get_pool() -> Result<Pool<Postgres>, sqlx::Error> {
     let db_url = std::env::var("DATABASE_URL")
         .expect("DATABASE_URL no configurada en el entorno");
     Pool::<Postgres>::connect(&db_url).await
-}
+}    
--- target/debug/deps/friendbank-9b5ab254c6599b0a.d
+++
@@ -1,5 +0,0 @@
-/Users/cesaralejandromorangraciano/Documents/Coding/rust/friendbank/target/debug/deps/friendbank-9b5ab254c6599b0a.d: src/main.rs
-
-/Users/cesaralejandromorangraciano/Documents/Coding/rust/friendbank/target/debug/deps/friendbank-9b5ab254c6599b0a: src/main.rs
-
-src/main.rs:
