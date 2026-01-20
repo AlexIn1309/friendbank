0a524baacdc549b2a0801ccc841116100dac54c5
AlexIn1309 <moran.graciano.cesar@gmail.com>
2026-01-20 00:50:26 -0600

    adding url database access

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,13 @@
 use sqlx::{Pool, Postgres};
 
 pub async fn get_pool() -> Result<Pool<Postgres>, sqlx::Error> {
-    match std::env::var("DATABASE_URL") {
-        Ok(db_url) => {
-            println!("DATABASE_URL presente: true");
-            Pool::<Postgres>::connect(&db_url).await
-        }
-        Err(e) => {
-            println!("DATABASE_URL presente: false -> {:?}", e);
-            Err(sqlx::Error::Configuration(Box::new(e)))
-        }
-    }
+    let db_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        // ⚠️ SOLO PARA DEBUG
+        "postgres://USER:PASSWORD@HOST:5432/DBNAME?sslmode=require".to_string()
+    });
+
+    println!("DB URL usada: {}", db_url.starts_with("postgres://"));
+
+    Pool::<Postgres>::connect(&db_url).await
 }
 
